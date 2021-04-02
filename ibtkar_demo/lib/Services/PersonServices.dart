e88eb488dcb9ad9ea3c0b5daf0b18b307bb55a28
Ahmed elbasha <64855329+AhmedElbasha97@users.noreply.github.com>
2021-04-03 00:10:59 +0200

    making services for the detail page

--- ibtkar_demo/lib/Services/PersonServices.dart
+++ ibtkar_demo/lib/Services/PersonServices.dart
@@ -1,6 +1,7 @@
 import 'dart:convert';
 
 import 'package:http/http.dart' as http;
+import 'package:ibtkar_demo/Models/PersonalDetailsModel.dart';
 import 'package:ibtkar_demo/Models/PersonsModel.dart';
 
 class PersonsService{
@@ -19,13 +20,13 @@ class PersonsService{
   }
 
 
-  Future<P> fetchMovie(int id) async {
+  Future<PersonalDetailsModel> fetchMovie(int id) async {
     var response = await http.get(
-        Uri.parse('https://api.themoviedb.org/3/movie/$id?api_key=6557d01ac95a807a036e5e9e325bb3f0&language=en-US'));
+        Uri.parse('https://api.themoviedb.org/3/person/$id?api_key=c949a0e09b06fbc87cacfbef4c504963&language=en-US'));
     if (response.statusCode == 200) {
-      var Movie =movieDetailsModelFromJson(response.body);
-      print("movie${Movie.title}");
-      return movieDetailsModelFromJson(response.body);
+      var person =personalDetailsModelFromJson(response.body);
+      print("movie${person.name}");
+      return personalDetailsModelFromJson(response.body);
     } else {
       throw Exception('FAILED TO LOAD THE MOVIE');
     }
