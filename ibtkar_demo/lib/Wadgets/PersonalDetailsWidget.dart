aab8827f983dce5e52072e21c948053a7b2e74e0
Ahmed elbasha <64855329+AhmedElbasha97@users.noreply.github.com>
2021-04-03 17:27:10 +0200

    check if there's image to display or not

--- ibtkar_demo/lib/Wadgets/PersonalDetailsWidget.dart
+++ ibtkar_demo/lib/Wadgets/PersonalDetailsWidget.dart
@@ -3,11 +3,17 @@ import 'package:flutter/material.dart';
 import 'package:ibtkar_demo/Models/PersonalDetailsModel.dart';
 import 'package:ibtkar_demo/UI/ImagesScreen/ImageScreen.dart';
 
-class PersonalDetailsWadgets extends StatelessWidget {
+class PersonalDetailsWadgets extends StatefulWidget {
   final PersonalDetailsModel PersonalDetails;
   final int index;
 
   const PersonalDetailsWadgets({Key key, this.PersonalDetails, this.index}) : super(key: key);
+
+  @override
+  _PersonalDetailsWadgetsState createState() => _PersonalDetailsWadgetsState();
+}
+
+class _PersonalDetailsWadgetsState extends State<PersonalDetailsWadgets> {
   @override
   Widget build(BuildContext context) {
     return Column(
@@ -15,20 +21,41 @@ class PersonalDetailsWadgets extends StatelessWidget {
       children: [
         GestureDetector(
           onTap:() {
-            if(PersonalDetails.profilePath != null) {
+            if(widget.PersonalDetails.profilePath != null) {
               print("hi");
               Navigator.of(context).push(MaterialPageRoute(
                   builder: (context) =>
-                      ImageScreen(id: PersonalDetails.id,
-                        imgPath: PersonalDetails.profilePath,)));
+                      ImageScreen(id: widget.PersonalDetails.id,
+                        imgPath: widget.PersonalDetails.profilePath,)));
+            }else{
+              showDialog<void>(
+                context: context,
+
+                // false = user must tap button, true = tap outside dialog
+                builder: (BuildContext dialogContext) {
+                  return AlertDialog(
+                    title: Text('cant Show Image'),
+                    content: Text("There's no image to display or download"),
+                    actions: <Widget>[
+                      FlatButton(
+                        child: Text('OK'),
+                        onPressed: () {
+                          Navigator.of(dialogContext)
+                              .pop(); // Dismiss alert dialog
+                        },
+                      ),
+                    ],
+                  );
+                },
+              );
             }
             },
           child: Hero(
-            tag: 'imageHero$index',
+            tag: 'imageHero${widget.index}',
 
              child:CachedNetworkImage(
                 imageUrl:
-                'https://image.tmdb.org/t/p/w500${PersonalDetails.profilePath}',
+                'https://image.tmdb.org/t/p/w500${widget.PersonalDetails.profilePath}',
                 imageBuilder: (context, imageProvider) => Container(
                 height: MediaQuery.of(context).size.height / 2,
                 width: MediaQuery.of(context).size.width / 1.5,
@@ -54,20 +81,20 @@ class PersonalDetailsWadgets extends StatelessWidget {
         Padding(
           padding: const EdgeInsets.symmetric(vertical: 20),
           child: Text(
-            '${PersonalDetails.name}',
+            '${widget.PersonalDetails.name}',
             style:
             TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
           ),
         ),
-        Text('Date Of Birth: ${PersonalDetails.birthday.day}/${PersonalDetails.birthday.month}/${PersonalDetails.birthday.year}'),
-        Text('Gender: ${PersonalDetails.gender}'),
-        Text('place Of Birth: ${PersonalDetails.placeOfBirth}'),
-        Text('Date Of death: ${PersonalDetails.deathday?? "Doesnt dead yet"}'),
+        Text('Date Of Birth: ${widget.PersonalDetails.birthday.day}/${widget.PersonalDetails.birthday.month}/${widget.PersonalDetails.birthday.year}'),
+        Text('Gender: ${widget.PersonalDetails.gender}'),
+        Text('place Of Birth: ${widget.PersonalDetails.placeOfBirth}'),
+        Text('Date Of death: ${widget.PersonalDetails.deathday?? "Doesnt dead yet"}'),
 
 
 
         Text(
-          '${PersonalDetails.biography}',
+          '${widget.PersonalDetails.biography}',
           textAlign: TextAlign.center,
         ),
 
